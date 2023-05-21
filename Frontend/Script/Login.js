8af2a44dcb8aa03ac17185c2b9d800c7224c50e5
raobaba <raorajan9576@gmail.com>
2023-05-21 15:14:46 +0530

    writing signup function

---
+++ Frontend/Script/Login.js
@@ -0,0 +1,27 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ username, email, password })
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log("Login successful!");
+        } else if (response.status === 401) {
+          console.log("User does not exist. Please create a new user.");
+        } else {
+          console.error("Error:", response.statusText);
+        }
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
+  });
--- Frontend/Script/Signup.js
+++ Frontend/Script/Signup.js
@@ -1,36 +1,48 @@
-const form = document.getElementById('signup-form');
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-   console.log('object')
+const submitData = async (event) => {
+  event.preventDefault(); // Prevent form submission
+  // Get form values
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
+  const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
-  const userPic = document.getElementById('user-pic').files[0];
+  const propic = document.getElementById('propic').value;
+  const friends = [];
+  const flowers = [];
+  const flowings = [];
+  const posts = []
+  // Create data object
+  const data = {
+    name,
+    email,
+    username,
+    password,
+    propic,
+    friends,
+    flowers,
+    flowings,
+    posts
+  };
 
-  const formData = new FormData();
-  formData.append('name', name);
-  formData.append('email', email);
-  formData.append('password', password);
-  formData.append('userPic', userPic);
-
-  fetch('http://localhost:8000/api/user', {
-    method: 'POST',
-    body: formData,
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-
-      return response.json();
-    })
-    .then((data) => {
-      console.log('User successfully created:', data);
-      // redirect to login page here
-      window.location.href = '/login.html';
-    })
-    .catch((error) => {
-      console.error('Error creating user:', error);
+  try {
+    const response = await fetch('http://localhost:8000/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
     });
-});
+
+    if (response.ok) {
+      alert('Data has been successfully Registered.');
+    } else {
+      alert("Error while Registeration")
+      console.log('Error posting data:', response.status);
+    }
+  } catch (error) {
+    console.log('Error:', error);
+  }
+};
+// Get the form element
+const form = document.getElementById('signup-form');
+// Attach event listener to form submission
+form.addEventListener('submit', submitData);
